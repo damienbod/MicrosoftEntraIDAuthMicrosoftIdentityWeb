5609c6ed11c7deb2abe7b10bfb2a4568d81313c6
damienbod <damien_bod@hotmail.com>
2020-11-01 18:21:54 +0100

    Adding swagger definitions

--- DownstreamApis/UserApiOne/Startup.cs
+++ DownstreamApis/UserApiOne/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.OpenApi.Models;
 using System;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace UserApiOne
 {
@@ -39,6 +40,27 @@ namespace UserApiOne
 
             services.AddSwaggerGen(c =>
             {
+                // add JWT Authentication
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Name = "JWT Authentication",
+                    Description = "Enter JWT Bearer token **_only_**",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer", // must be lower case
+                    BearerFormat = "JWT",
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+                c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {securityScheme, new string[] { }}
+                });
+
                 c.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Title = "User API One",
