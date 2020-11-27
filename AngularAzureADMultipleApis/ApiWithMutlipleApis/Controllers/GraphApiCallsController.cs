b0990834334aea57b70361067708e97892d9f36b
damienbod <damien_bod@hotmail.com>
2020-11-27 11:36:48 +0100

    updating Angular APP

---
+++ AngularAzureADMultipleApis/ApiWithMutlipleApis/Controllers/GraphApiCallsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiWithMutlipleApis.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class GraphApiCallsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new List<string> { "some data", "more data", "loads of data" };
+        }
+    }
+}
