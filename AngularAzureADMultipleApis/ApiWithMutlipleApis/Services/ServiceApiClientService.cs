bd2155962eb2f7b0a1c9e78920512f5a620aab9f
damienbod <damien_bod@hotmail.com>
2020-11-27 15:49:53 +0100

    updating structure of projects

--- AngularAzureADMultipleApis/ApiWithMutlipleApis/Services/ServiceApiClientService.cs
+++ AngularAzureADMultipleApis/ApiWithMutlipleApis/Services/ServiceApiClientService.cs
@@ -29,7 +29,7 @@ namespace ApiWithMutlipleApis.Services
             var scope = "api://b178f3a5-7588-492a-924f-72d7887b7e48/access_as_application";
             var accessToken = await _tokenAcquisition.GetAccessTokenForAppAsync(scope);
 
-            client.BaseAddress = new Uri("https://localhost:44391");
+            client.BaseAddress = new Uri("https://localhost:44324");
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
