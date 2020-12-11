7b6a9a9d3c48e86faedcfb8ec6c5cf251235c147
damienbod <damien_bod@hotmail.com>
2020-12-11 19:21:32 +0100

    updating AAD auth client

--- BlazorWithApis/BlazorAzureADWithApis/Client/Program.cs
+++ BlazorWithApis/BlazorAzureADWithApis/Client/Program.cs
@@ -27,7 +27,7 @@ namespace BlazorAzureADWithApis.Client
             builder.Services.AddMsalAuthentication(options =>
             {
                 builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-                options.ProviderOptions.DefaultAccessTokenScopes.Add("api://blazorazureadwithapis_server/user_impersonation");
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("api://2b50a014-f353-4c10-aace-024f19a55569/access_as_user");
             });
 
             await builder.Build().RunAsync();
