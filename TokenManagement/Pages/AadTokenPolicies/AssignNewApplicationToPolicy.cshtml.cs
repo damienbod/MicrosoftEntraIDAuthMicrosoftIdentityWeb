67f3e6999472f3df70c9ec098f43225c86300316
Damien <damien_bod@hotmail.com>
2021-01-02 16:45:34 +0100

    styling

--- TokenManagement/Pages/AadTokenPolicies/AssignNewApplicationToPolicy.cshtml.cs
+++ TokenManagement/Pages/AadTokenPolicies/AssignNewApplicationToPolicy.cshtml.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
