2397e034ba50cba1137fbe7dfe7d796ca30faf41
damienbod <damien_bod@hotmail.com>
2021-01-19 10:34:52 +0100

    Updating key vault access

--- ConfidentialClientCredentialsCertificate/MyServerRenderedPortal/CallApi/ApiService.cs
+++ ConfidentialClientCredentialsCertificate/MyServerRenderedPortal/CallApi/ApiService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.KeyVault;
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Certificates;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -33,12 +34,10 @@ namespace MyServerRenderedPortal
             {
                 // Use Key Vault to get certificate
                 var azureServiceTokenProvider = new AzureServiceTokenProvider();
-                // using managed identities
-                var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
 
                 // Get the certificate from Key Vault
                 var identifier = _configuration["CallApi:ClientCertificates:0:KeyVaultCertificateName"];
-                var cert = await GetCertificateAsync(identifier, kv);
+                var cert = await GetCertificateAsync(identifier);
 
                 var client = _clientFactory.CreateClient();
 
@@ -77,15 +76,18 @@ namespace MyServerRenderedPortal
             }
         }
 
-        private async Task<X509Certificate2> GetCertificateAsync(string identitifier, KeyVaultClient keyVaultClient)
+        private async Task<X509Certificate2> GetCertificateAsync(string identitifier)
         {
             var vaultBaseUrl = _configuration["CallApi:ClientCertificates:0:KeyVaultUrl"];
+            var client = new CertificateClient(vaultUri: new Uri(vaultBaseUrl), credential: new DefaultAzureCredential());
+            KeyVaultCertificateWithPolicy certificateWithPolicy = client.GetCertificate(identitifier);
 
-            var certificateVersionBundle = await keyVaultClient.GetCertificateAsync(vaultBaseUrl, identitifier);
-            var certificatePrivateKeySecretBundle = await keyVaultClient.GetSecretAsync(certificateVersionBundle.SecretIdentifier.Identifier);
-            var privateKeyBytes = Convert.FromBase64String(certificatePrivateKeySecretBundle.Value);
-            var certificateWithPrivateKey = new X509Certificate2(privateKeyBytes, (string)null, X509KeyStorageFlags.MachineKeySet);
-            return certificateWithPrivateKey;
+
+            //var certificateVersionBundle = await keyVaultClient.GetCertificateAsync(vaultBaseUrl, identitifier);
+            //var certificatePrivateKeySecretBundle = await keyVaultClient.GetSecretAsync(certificateVersionBundle.SecretIdentifier.Identifier);
+            //var privateKeyBytes = Convert.FromBase64String(certificatePrivateKeySecretBundle.Value);
+            //var certificateWithPrivateKey = new X509Certificate2(privateKeyBytes, (string)null, X509KeyStorageFlags.MachineKeySet);
+            return null;  // certificateWithPrivateKey;
         }
 
         void MyLoggingMethod(Microsoft.Identity.Client.LogLevel level, string message, bool containsPii)
