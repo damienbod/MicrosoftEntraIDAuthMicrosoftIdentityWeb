352dc87f470aa37ad25c06e0de05215f077bb1fe
damienbod <damien_bod@hotmail.com>
2021-03-02 21:24:38 +0100

    code clean up

--- BlazorBff/BlazorAzureAdBff/Server/Controllers/AccountController.cs
+++ BlazorBff/BlazorAzureAdBff/Server/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorAzureADWithApis.Server.Controllers
@@ -11,13 +12,20 @@ namespace BlazorAzureADWithApis.Server.Controllers
         [HttpGet("Login")]
         public ActionResult Login(string returnUrl)
         {
-            return Challenge(new AuthenticationProperties { RedirectUri = !string.IsNullOrEmpty(returnUrl) ? returnUrl : "/" });
+            return Challenge(new AuthenticationProperties 
+            { 
+                RedirectUri = !string.IsNullOrEmpty(returnUrl) ? returnUrl : "/" 
+            });
         }
 
+        [Authorize]
         [HttpGet("Logout")]
-        public IActionResult Logout() => SignOut(
-            new AuthenticationProperties { RedirectUri = "/" },
-            CookieAuthenticationDefaults.AuthenticationScheme,
-            OpenIdConnectDefaults.AuthenticationScheme);
+        public IActionResult Logout()
+        {
+            return SignOut(
+                new AuthenticationProperties { RedirectUri = "/" },
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                OpenIdConnectDefaults.AuthenticationScheme);
+        }
     }
 }
--- BlazorBff/readme.md
+++ BlazorBff/readme.md
@@ -1,11 +1,20 @@
 
+# Credits
 
-#Links
+A lot of code in this repo was built using original code src from: 
+
+Author: Bernd Hirschmann
+
+https://github.com/berhir/BlazorWebAssemblyCookieAuth
+
+https://twitter.com/berhir
+
+# Links
 
 https://github.com/berhir/BlazorWebAssemblyCookieAuth
 
 https://guidnew.com/en/blog/secure-a-blazor-webassembly-application-with-cookie-authentication/
 
-https://docs.microsoft.com/en-us/aspnet/core/security/anti-request-forgery?view=aspnetcore-5.0
+https://docs.microsoft.com/en-us/aspnet/core/security/anti-request-forgery
 
-https://docs.microsoft.com/en-us/aspnet/core/blazor/security/?view=aspnetcore-5.0
+https://docs.microsoft.com/en-us/aspnet/core/blazor/security
