fe278be9102cc27c459558717877b008e516feb6
damienbod <damien_bod@hotmail.com>
2021-11-04 13:54:19 +0100

    updating libs

--- DecryptionCertificates/PortalDecryptionCertificates/CallApi/ApiService.cs
+++ DecryptionCertificates/PortalDecryptionCertificates/CallApi/ApiService.cs
@@ -42,6 +42,10 @@ namespace PortalDecryptionCertificates
 
                 return data;
             }
+            else
+            {
+                var error = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            }
 
             throw new ApplicationException($"Status code: {response.StatusCode}, Error: {response.ReasonPhrase}");
         }
