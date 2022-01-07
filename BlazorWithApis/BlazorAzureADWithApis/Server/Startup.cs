df2221921985d1a53844f5765c087207cd8b645a
damienbod <damien_bod@hotmail.com>
2022-01-07 12:41:02 +0100

    update projects

--- BlazorWithApis/BlazorAzureADWithApis/Server/Startup.cs
+++ BlazorWithApis/BlazorAzureADWithApis/Server/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Identity.Web;
-using System.Linq;
 
 namespace BlazorAzureADWithApis.Server
 {
