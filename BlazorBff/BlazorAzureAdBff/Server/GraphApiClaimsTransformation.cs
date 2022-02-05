f5a1f029f4b060ebc6c06683c922bfc5874012a8
damienbod <damien_bod@hotmail.com>
2022-02-05 16:20:34 +0100

    update

--- BlazorBff/BlazorAzureAdBff/Server/GraphApiClaimsTransformation.cs
+++ BlazorBff/BlazorAzureAdBff/Server/GraphApiClaimsTransformation.cs
@@ -44,7 +44,7 @@ namespace BlazorAzureADWithApis.Server
             Claim claim = groupId switch
             {
                 "1d9fba7e-b98a-45ec-b576-7ee77366cf10" => new Claim(Constants.DemoUsersIdentifier, Constants.DemoUsersValue),
-                "be30f1dd-39c9-457b-ab22-55f5b67fb566" => new Claim(Constants.DemoAdminsIdentifier, Constants.DemoAdminsValue)
+                "be30f1dd-39c9-457b-ab22-55f5b67fb566" => new Claim(Constants.DemoAdminsIdentifier, Constants.DemoAdminsValue),
                 _ => null
             };
 
