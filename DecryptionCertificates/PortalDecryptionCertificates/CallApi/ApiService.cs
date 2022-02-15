0d717f4ac0cf8fc27d3c5b8a7a6f4772830b79fd
damienbod <damien_bod@hotmail.com>
2022-02-15 21:06:06 +0100

    Updated namespaces

--- DecryptionCertificates/PortalDecryptionCertificates/CallApi/ApiService.cs
+++ DecryptionCertificates/PortalDecryptionCertificates/CallApi/ApiService.cs
@@ -6,44 +6,43 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
-namespace PortalDecryptionCertificates
+namespace PortalDecryptionCertificates;
+
+public class ApiService
 {
-    public class ApiService
+    private readonly IHttpClientFactory _clientFactory;
+    private readonly ITokenAcquisition _tokenAcquisition;
+    private readonly IConfiguration _configuration;
+
+    public ApiService(IHttpClientFactory clientFactory, 
+        ITokenAcquisition tokenAcquisition, 
+        IConfiguration configuration)
     {
-        private readonly IHttpClientFactory _clientFactory;
-        private readonly ITokenAcquisition _tokenAcquisition;
-        private readonly IConfiguration _configuration;
+        _clientFactory = clientFactory;
+        _tokenAcquisition = tokenAcquisition;
+        _configuration = configuration;
+    }
 
-        public ApiService(IHttpClientFactory clientFactory, 
-            ITokenAcquisition tokenAcquisition, 
-            IConfiguration configuration)
+    public async Task<JArray> GetApiDataAsync()
+    {
+        var client = _clientFactory.CreateClient();
+
+        var scope = _configuration["CallApi:ScopeForAccessToken"];
+        var accessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(new[] { scope });
+
+        client.BaseAddress = new Uri(_configuration["CallApi:ApiBaseAddress"]);
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        var response = await client.GetAsync("weatherforecast");
+        if (response.IsSuccessStatusCode)
         {
-            _clientFactory = clientFactory;
-            _tokenAcquisition = tokenAcquisition;
-            _configuration = configuration;
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var data = JArray.Parse(responseContent);
+
+            return data;
         }
 
-        public async Task<JArray> GetApiDataAsync()
-        {
-            var client = _clientFactory.CreateClient();
-
-            var scope = _configuration["CallApi:ScopeForAccessToken"];
-            var accessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(new[] { scope }).ConfigureAwait(false);
-
-            client.BaseAddress = new Uri(_configuration["CallApi:ApiBaseAddress"]);
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            var response = await client.GetAsync("weatherforecast").ConfigureAwait(false);
-            if (response.IsSuccessStatusCode)
-            {
-                var responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                var data = JArray.Parse(responseContent);
-
-                return data;
-            }
-
-            throw new ApplicationException($"Status code: {response.StatusCode}, Error: {response.ReasonPhrase}");
-        }
+        throw new ApplicationException($"Status code: {response.StatusCode}, Error: {response.ReasonPhrase}");
     }
 }
