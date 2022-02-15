9874cf0cb8dde8d68bcc93441c8dd6b9be7afa3a
damienbod <damien_bod@hotmail.com>
2022-02-15 20:32:03 +0100

    Update applications

--- AppRoles/WebAppWithRoles/ClientApiWithRoles/ClientApiWithRolesService.cs
+++ AppRoles/WebAppWithRoles/ClientApiWithRoles/ClientApiWithRolesService.cs
@@ -7,63 +7,60 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
-namespace WebAppWithRoles
+namespace WebAppWithRoles;
+
+public class ClientApiWithRolesService
 {
-    public class ClientApiWithRolesService
+    private readonly IHttpClientFactory _clientFactory;
+    private readonly ITokenAcquisition _tokenAcquisition;
+    private readonly IConfiguration _configuration;
+
+    public ClientApiWithRolesService(IHttpClientFactory clientFactory, 
+        ITokenAcquisition tokenAcquisition, 
+        IConfiguration configuration)
     {
-        private readonly IHttpClientFactory _clientFactory;
-        private readonly ITokenAcquisition _tokenAcquisition;
-        private readonly IConfiguration _configuration;
+        _clientFactory = clientFactory;
+        _tokenAcquisition = tokenAcquisition;
+        _configuration = configuration;
+    }
 
-        public ClientApiWithRolesService(IHttpClientFactory clientFactory, 
-            ITokenAcquisition tokenAcquisition, 
-            IConfiguration configuration)
+    public async Task<JArray> GetUserDataFromApi()
+    {
+        return await GetDataFromApi("userdata");
+    }
+
+    public async Task<JArray> GetStudentDataFromApi()
+    {
+        return await GetDataFromApi("studentdata");
+    }
+
+    public async Task<JArray> GetAdminDataFromApi()
+    {
+        return await GetDataFromApi("admindata");
+    }
+
+
+    private async Task<JArray> GetDataFromApi(string path)
+    {
+        var client = _clientFactory.CreateClient();
+
+        var scope = _configuration["ApiWithRoles:ScopeForAccessToken"];
+        var accessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(new[] { scope });
+
+        client.BaseAddress = new Uri(_configuration["ApiWithRoles:ApiBaseAddress"]);
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        var response = await client.GetAsync($"api/{path}");
+        if (response.IsSuccessStatusCode)
         {
-            _clientFactory = clientFactory;
-            _tokenAcquisition = tokenAcquisition;
-            _configuration = configuration;
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            var data = JArray.Parse(responseContent);
+
+            return data;
         }
-
-        public async Task<JArray> GetUserDataFromApi()
-        {
-            return await GetDataFromApi("userdata").ConfigureAwait(false);
-        }
-
-        public async Task<JArray> GetStudentDataFromApi()
-        {
-            return await GetDataFromApi("studentdata").ConfigureAwait(false);
-        }
-
-        public async Task<JArray> GetAdminDataFromApi()
-        {
-            return await GetDataFromApi("admindata").ConfigureAwait(false);
-        }
-
-
-        private async Task<JArray> GetDataFromApi(string path)
-        {
-            var client = _clientFactory.CreateClient();
-
-            var scope = _configuration["ApiWithRoles:ScopeForAccessToken"];
-            var accessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(new[] { scope })
-                .ConfigureAwait(false);
-
-            client.BaseAddress = new Uri(_configuration["ApiWithRoles:ApiBaseAddress"]);
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            var response = await client.GetAsync($"api/{path}");
-            if (response.IsSuccessStatusCode)
-            {
-                var responseContent = await response.Content.ReadAsStringAsync()
-                    .ConfigureAwait(false);
-
-                var data = JArray.Parse(responseContent);
-
-                return data;
-            }
-            var errorList = new List<string> { $"Status code: {response.StatusCode}", $"Error: {response.ReasonPhrase}" };
-            return JArray.FromObject(errorList);
-        }
+        var errorList = new List<string> { $"Status code: {response.StatusCode}", $"Error: {response.ReasonPhrase}" };
+        return JArray.FromObject(errorList);
     }
 }
