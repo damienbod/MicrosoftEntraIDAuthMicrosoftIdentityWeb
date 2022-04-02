956e3ef04d547f8d7e854c45fa123774d1661048
damienbod <damien_bod@hotmail.com>
2022-04-02 14:56:24 +0200

    remove startup file and using only program

--- DownstreamApis/UserApiOne/Program.cs
+++ DownstreamApis/UserApiOne/Program.cs
@@ -1,19 +1,101 @@
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using UserApiOne;
 
-namespace UserApiOne;
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<UserApiTwoService>();
+builder.Services.AddHttpClient();
+builder.Services.AddOptions();
+
+builder.Services.AddMicrosoftIdentityWebApiAuthentication(builder.Configuration, "AzureAd")
+    .EnableTokenAcquisitionToCallDownstreamApi()
+    .AddInMemoryTokenCaches();
+
+builder.Services.AddSwaggerGen(c =>
 {
-    public static void Main(string[] args)
+    // add JWT Authentication
+    var securityScheme = new OpenApiSecurityScheme
     {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
+        Name = "JWT Authentication",
+        Description = "Enter JWT Bearer token **_only_**",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer", // must be lower case
+        BearerFormat = "JWT",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+    c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
             {
-                webBuilder.UseStartup<Startup>();
+                {securityScheme, Array.Empty<string>()}
             });
+
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "User API One",
+        Version = "v1",
+        Description = "User API One",
+        Contact = new OpenApiContact
+        {
+            Name = "damienbod",
+            Email = string.Empty,
+            Url = new Uri("https://damienbod.com/"),
+        },
+    });
+});
+
+builder.Services.AddControllers(options =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        // .RequireClaim("email") // disabled this to test with users that have no email (no license added)
+        .Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "User API Two");
+    c.RoutePrefix = string.Empty;
+});
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
