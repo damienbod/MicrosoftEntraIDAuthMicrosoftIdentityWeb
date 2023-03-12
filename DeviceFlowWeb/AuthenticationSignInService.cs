de99d34d927bff396131be737395d680dee66324
damienbod <damien_bod@hotmail.com>
2023-03-12 17:12:27 +0100

    Update .NET 7 styles

--- DeviceFlowWeb/AuthenticationSignInService.cs
+++ DeviceFlowWeb/AuthenticationSignInService.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Threading.Tasks;
 
 namespace DeviceFlowWeb;
 
