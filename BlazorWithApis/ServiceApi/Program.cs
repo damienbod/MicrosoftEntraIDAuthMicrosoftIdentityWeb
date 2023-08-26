7b29f86830a187cb735b3ffc86e53e65785f2041
damienbod <damien_bod@hotmail.com>
2023-08-26 09:22:18 +0200

    Update start code

--- BlazorWithApis/ServiceApi/Program.cs
+++ BlazorWithApis/ServiceApi/Program.cs
@@ -1,47 +1,95 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.OpenApi.Models;
 using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.SystemConsole.Themes;
+using ServiceApi;
+using System.IdentityModel.Tokens.Jwt;
 
-namespace ServiceApi;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Services.AddSingleton<IAuthorizationHandler, HasServiceApiRoleHandler>();
+builder.Services.AddMicrosoftIdentityWebApiAuthentication(builder.Configuration);
+builder.Services.AddControllers();
+
+builder.Services.AddAuthorization(options =>
 {
-    public static int Main(string[] args)
+    options.AddPolicy("ValidateAccessTokenPolicy", validateAccessTokenPolicy =>
     {
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()
-            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-            .Enrich.FromLogContext()
-            .WriteTo.Console()
-            .CreateLogger();
+        validateAccessTokenPolicy.Requirements.Add(new HasServiceApiRoleRequirement());
 
-        try
-        {
-            Log.Information("Starting web host");
-            CreateHostBuilder(args).Build().Run();
-            return 0;
-        }
-        catch (Exception ex)
-        {
-            Log.Fatal(ex, "Host terminated unexpectedly");
-            return 1;
-        }
-        finally
-        {
-            Log.CloseAndFlush();
-        }
-    }
+        // Validate id of application for which the token was created
+        // In this case the UI application 
+        validateAccessTokenPolicy.RequireClaim("azp", "2b50a014-f353-4c10-aace-024f19a55569");
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
-                .ReadFrom.Configuration(hostingContext.Configuration)
-                .Enrich.FromLogContext()
-                .WriteTo.File("../LogsServiceApi.txt")
-                .WriteTo.Console(theme: AnsiConsoleTheme.Code)
-            )
-            .ConfigureWebHostDefaults(webBuilder =>
+        // only allow tokens which used "Private key JWT Client authentication"
+        // // https://docs.microsoft.com/en-us/azure/active-directory/develop/access-tokens
+        // Indicates how the client was authenticated. For a public client, the value is "0". 
+        // If client ID and client secret are used, the value is "1". 
+        // If a client certificate was used for authentication, the value is "2".
+        validateAccessTokenPolicy.RequireClaim("azpacr", "1");
+    });
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    // add JWT Authentication
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "JWT Authentication",
+        Description = "Enter JWT Bearer token **_only_**",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer", // must be lower case
+        BearerFormat = "JWT",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+    c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
             {
-                webBuilder.UseStartup<Startup>();
+                {securityScheme, Array.Empty<string>()}
             });
-}
+
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Service API One",
+        Version = "v1",
+        Description = "User API One",
+        Contact = new OpenApiContact
+        {
+            Name = "damienbod",
+            Email = string.Empty,
+            Url = new Uri("https://damienbod.com/"),
+        },
+    });
+});
+var app = builder.Build();
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+IdentityModelEventSource.ShowPII = true;
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Service API One");
+    c.RoutePrefix = string.Empty;
+});
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
