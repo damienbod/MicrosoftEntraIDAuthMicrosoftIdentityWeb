394c20685c603c476b4e7cc0664e071914a4a9e7
damienbod <damien_bod@hotmail.com>
2023-08-26 10:26:16 +0200

    Improve startup

--- AppRoles/WebAppWithRoles/Program.cs
+++ AppRoles/WebAppWithRoles/Program.cs
@@ -1,16 +1,70 @@
-namespace WebAppWithRoles;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using System.IdentityModel.Tokens.Jwt;
+using WebAppWithRoles;
 
-public class Program
-{
-    public static void Main(string[] args)
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<ClientApiWithRolesService>();
+builder.Services.AddHttpClient();
+
+builder.Services.AddOptions();
+
+builder.Services.AddDistributedMemoryCache();
+string[]? initialScopes = builder.Configuration.GetValue<string>(
+    "ApiWithRoles:ScopeForAccessToken")?.Split(' ');
+
+//builder.Services.AddMicrosoftIdentityWebAppAuthentication(Configuration)
+//    .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
+//    .AddDistributedTokenCaches();
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(options =>
     {
-        CreateHostBuilder(args).Build().Run();
-    }
+        builder.Configuration.Bind("AzureAd", options);
+        options.UsePkce = true;
+    },
+        options => { builder.Configuration.Bind("AzureAd", options); }
+    )
+    .EnableTokenAcquisitionToCallDownstreamApi(
+        options => builder.Configuration.Bind("AzureAd", options), initialScopes)
+    .AddDistributedTokenCaches();
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+builder.Services.AddRazorPages().AddMvcOptions(options =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+}).AddMicrosoftIdentityUI();
+
+var app = builder.Build();
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+// IdentityModelEventSource.ShowPII = true;
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+
+app.Run();
