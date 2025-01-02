7560d1beb3f341e848bb2550dd542a4a874708ec
damienbod <damien_bod@hotmail.com>
2025-01-02 14:50:36 +0100

    Small updates

--- BlazorWithApis/ServiceApi/Program.cs
+++ BlazorWithApis/ServiceApi/Program.cs
@@ -1,92 +1,34 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.Identity.Web;
-using Microsoft.IdentityModel.Logging;
-using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Builder;
+using Serilog;
 using ServiceApi;
-using System.IdentityModel.Tokens.Jwt;
+using System;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddSingleton<IAuthorizationHandler, HasServiceApiRoleHandler>();
-builder.Services.AddMicrosoftIdentityWebApiAuthentication(builder.Configuration);
-builder.Services.AddControllers();
+Log.Information("Starting ServiceApi application");
 
-builder.Services.AddAuthorization(options =>
+try
 {
-    options.AddPolicy("ValidateAccessTokenPolicy", validateAccessTokenPolicy =>
-    {
-        validateAccessTokenPolicy.Requirements.Add(new HasServiceApiRoleRequirement());
+    var builder = WebApplication.CreateBuilder(args);
 
-        // Validate id of application for which the token was created
-        // In this case the UI application 
-        validateAccessTokenPolicy.RequireClaim("azp", "2b50a014-f353-4c10-aace-024f19a55569");
+    builder.Host.UseSerilog((context, loggerConfiguration) => loggerConfiguration
+        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+        .ReadFrom.Configuration(context.Configuration));
 
-        // only allow tokens which used "Private key JWT Client authentication"
-        // // https://docs.microsoft.com/en-us/azure/active-directory/develop/access-tokens
-        // Indicates how the client was authenticated. For a public client, the value is "0". 
-        // If client ID and client secret are used, the value is "1". 
-        // If a client certificate was used for authentication, the value is "2".
-        validateAccessTokenPolicy.RequireClaim("azpacr", "1");
-    });
-});
+    var app = builder
+        .ConfigureServices()
+        .ConfigurePipeline();
 
-builder.Services.AddSwaggerGen(c =>
+    app.Run();
+}
+catch (Exception ex) when (ex.GetType().Name is not "StopTheHostException" && ex.GetType().Name is not "HostAbortedException")
 {
-    // add JWT Authentication
-    var securityScheme = new OpenApiSecurityScheme
-    {
-        Name = "JWT Authentication",
-        Description = "Enter JWT Bearer token **_only_**",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer", // must be lower case
-        BearerFormat = "JWT",
-        Reference = new OpenApiReference
-        {
-            Id = JwtBearerDefaults.AuthenticationScheme,
-            Type = ReferenceType.SecurityScheme
-        }
-    };
-    c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
-                {securityScheme, Array.Empty<string>()}
-            });
-
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Service API One",
-        Version = "v1",
-        Description = "User API One",
-        Contact = new OpenApiContact
-        {
-            Name = "damienbod",
-            Email = string.Empty,
-            Url = new Uri("https://damienbod.com/"),
-        },
-    });
-});
-var app = builder.Build();
-
-JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-IdentityModelEventSource.ShowPII = true;
-JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
-
-app.UseSwagger();
-app.UseSwaggerUI(c =>
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Service API One");
-    c.RoutePrefix = string.Empty;
-});
-
-app.UseHttpsRedirection();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
