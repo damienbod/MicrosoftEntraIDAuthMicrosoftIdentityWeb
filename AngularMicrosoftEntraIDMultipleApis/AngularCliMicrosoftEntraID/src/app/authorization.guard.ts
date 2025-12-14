91e73426266e7cd2eea23842366976e57fc45ca1
damienbod <damien_bod@hotmail.com>
2025-12-14 20:35:28 +0100

    Update Angular

--- AngularMicrosoftEntraIDMultipleApis/AngularCliMicrosoftEntraID/src/app/authorization.guard.ts
+++ AngularMicrosoftEntraIDMultipleApis/AngularCliMicrosoftEntraID/src/app/authorization.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
-export class AuthorizationGuard implements CanActivate {
+export class AuthorizationGuard  {
     constructor(private oidcSecurityService: OidcSecurityService, private router: Router) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
