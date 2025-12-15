d095726b0f8678c467b44291f2176989c42c7258
damienbod <damien_bod@hotmail.com>
2025-12-15 11:57:27 +0100

    namespaces

--- DownstreamApis/WebAppUserApis/BearerSecuritySchemeTransformer.cs
+++ DownstreamApis/WebAppUserApis/BearerSecuritySchemeTransformer.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.OpenApi;
 using Microsoft.OpenApi;
 
-namespace UserApi;
+namespace WebAppUserApis;
 
 internal sealed class BearerSecuritySchemeTransformer(IAuthenticationSchemeProvider authenticationSchemeProvider) : IOpenApiDocumentTransformer
 {
